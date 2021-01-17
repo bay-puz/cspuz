5cd18ce23fc6da51ae0110745c949fab1e88fbef
bay-puz <68962000+bay-puz@users.noreply.github.com>
2021-01-18 02:01:27 +0900

    make amarune solver (using z3)

---
+++ cspuz/puzzle/amarune.py
@@ -0,0 +1,77 @@
+import sys
+
+from cspuz import Solver, alldifferent
+
+
+def solve_amarune(height, width, problem):
+    solver = Solver()
+    numbers = solver.int_array((height, width), 1, height * width)
+    solver.add_answer_key(numbers)
+
+    solver.ensure(alldifferent(numbers))
+
+    for y in range(height):
+        for x in range(width-1):
+            solver.ensure(numbers[y, x] > numbers[y, x+1])
+
+    for y in range(height):
+        for x in range(width):
+            if problem[y][x] > 0:
+                solver.ensure(numbers[y, x] == problem[y][x])
+
+    for y in range(height):
+        for x in range(width-1):
+            if problem[y+height][x] in [0, 1, 2]:
+                solver.ensure(numbers[y, x] % numbers[y, x+1] == problem[y+height][x])
+            elif problem[y+height][x] == 3:
+                solver.ensure(numbers[y, x] % numbers[y, x+1] >= 3)
+
+    for y in range(height-1):
+        for x in range(width):
+            if problem[y+height*2][x] in [0, 1, 2]:
+                solver.ensure((numbers[y, x] > numbers[y+1, x]).then(numbers[y, x] % numbers[y+1, x] == problem[y+height*2][x]))
+                solver.ensure((numbers[y+1, x] > numbers[y, x]).then(numbers[y+1, x] % numbers[y, x] == problem[y+height*2][x]))
+            elif problem[y+height*2][x] == 3:
+                solver.ensure((numbers[y, x] > numbers[y+1, x]).then(numbers[y, x] % numbers[y+1, x] >= 3))
+                solver.ensure((numbers[y+1, x] > numbers[y, x]).then(numbers[y+1, x] % numbers[y, x] >= 3))
+
+    from cspuz.backend import z3
+    is_sat = solver.solve(z3)
+    return is_sat, numbers
+
+
+def _main():
+    # https://puzsq.jp/main/puzzle_play.php?pid=2866
+    height = 3
+    width = 5
+    problem = [
+        [ 0,  0,  7,  0,  0],
+        [ 0,  0,  0,  0,  0],
+        [ 0,  9,  0,  0,  0],
+        [-1,  0, -1,  1],
+        [-1,  1, -1, -1],
+        [ 3,  0, -1, -1],
+        [ 2, -1, -1, -1,  2],
+        [-1, -1, -1,  0, -1],
+    ]
+
+    is_sat, answer = solve_amarune(height, width, problem)
+
+    if not is_sat:
+        print('no answer', file=sys.stderr)
+        return
+
+    print('answer:')
+    for y in range(height):
+        for x in range(width):
+            if answer[y, x].sol is None:
+                print('?', end=' ')
+            else:
+                if answer[y, x].sol < 10:
+                    print(' ', end='')
+                print(answer[y, x].sol, end=' ')
+        print('')
+
+
+if __name__ == '__main__':
+    _main()
