e0ab15ebf53190865f07c0516daceaba5b29401f
bay-puz <68962000+bay-puz@users.noreply.github.com>
2021-01-18 22:35:35 +0900

    make amarune generator

--- cspuz/puzzle/amarune.py
+++ cspuz/puzzle/amarune.py
@@ -1,6 +1,7 @@
 import sys
 
 from cspuz import Solver, alldifferent
+from cspuz.generator import generate_problem, count_non_default_values, Choice
 
 
 def solve_amarune(height, width, problem):
@@ -41,37 +42,87 @@ def solve_amarune(height, width, problem):
     return is_sat, numbers
 
 
+def generate_amarune(height, width, verbose=True):
+    pattern = []
+    cand = Choice([-1] + list(range(1, height * width - 1)), -1)
+    pattern += [[cand for _ in range(width)] for _ in range(height)]
+    cand = Choice([-1, 0, 1, 2, 3], -1)
+    pattern += [[cand for _ in range(width-1)] for _ in range(height)]
+    pattern += [[cand for _ in range(width)] for _ in range(height-1)]
+
+    def pretest(problem):
+        count_number_hint = 0
+        for y in range(height):
+            for x in range(width):
+                if problem[y][x] > 0:
+                    count_number_hint += 1
+        return (count_number_hint < 3)
+
+    generated = generate_problem(lambda problem: solve_amarune(height, width, problem),
+                                 builder_pattern=pattern,
+                                 clue_penalty=lambda problem: count_non_default_values(problem, default=-1, weight=2),
+                                 pretest=pretest,
+                                 verbose=verbose)
+    return generated
+
+
 def _main():
-    # https://puzsq.jp/main/puzzle_play.php?pid=2866
-    height = 3
-    width = 5
-    problem = [
-        [ 0,  0,  7,  0,  0],
-        [ 0,  0,  0,  0,  0],
-        [ 0,  9,  0,  0,  0],
-        [-1,  0, -1,  1],
-        [-1,  1, -1, -1],
-        [ 3,  0, -1, -1],
-        [ 2, -1, -1, -1,  2],
-        [-1, -1, -1,  0, -1],
-    ]
+    if len(sys.argv) == 1:
+        # https://puzsq.jp/main/puzzle_play.php?pid=2866
+        height = 3
+        width = 5
+        problem = [
+            [ 0,  0,  7,  0,  0],
+            [ 0,  0,  0,  0,  0],
+            [ 0,  9,  0,  0,  0],
+            [-1,  0, -1,  1],
+            [-1,  1, -1, -1],
+            [ 3,  0, -1, -1],
+            [ 2, -1, -1, -1,  2],
+            [-1, -1, -1,  0, -1],
+        ]
 
-    is_sat, answer = solve_amarune(height, width, problem)
+        is_sat, answer = solve_amarune(height, width, problem)
 
-    if not is_sat:
-        print('no answer', file=sys.stderr)
-        return
+        if not is_sat:
+            print('no answer', file=sys.stderr)
+            return
 
-    print('answer:')
-    for y in range(height):
-        for x in range(width):
-            if answer[y, x].sol is None:
-                print('?', end=' ')
-            else:
-                if answer[y, x].sol < 10:
-                    print(' ', end='')
-                print(answer[y, x].sol, end=' ')
-        print('')
+        print('answer:')
+        for y in range(height):
+            for x in range(width):
+                if answer[y, x].sol is None:
+                    print('?', end=' ')
+                else:
+                    if answer[y, x].sol < 10:
+                        print(' ', end='')
+                    print(answer[y, x].sol, end=' ')
+            print('')
+
+    elif len(sys.argv) > 2:
+        height, width = map(int, sys.argv[1:3])
+        gen = generate_amarune(height, width)
+        if gen is None:
+            print('not generated')
+            return
+        for row in gen:
+            for c in row:
+                if c == -1:
+                    print('-', end=' ')
+                else:
+                    print(c, end=' ')
+            print('')
+
+        is_sat, answer = solve_amarune(height, width, gen)
+        for y in range(height):
+            for x in range(width):
+                if answer[y, x].sol is None:
+                    print('?', end=' ')
+                else:
+                    if answer[y, x].sol < 10:
+                        print(' ', end='')
+                    print(answer[y, x].sol, end=' ')
+            print('')
 
 
 if __name__ == '__main__':
